19d80736b4dbd4358498a3d407d8ed05e5975183
Abdella <abdella.shukur2@gmail.com>
2020-04-13 01:08:33 -0400

    update

--- public/app2.js
+++ public/app2.js
@@ -1,12 +1,12 @@
 
-
-const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
 const random = Math.floor(Math.random() * 4) + 1;
 
-button1.addEventListener('click', function () {
-    axios.get('/names')
+
+button2.addEventListener('click', function () {
+    axios.get('/girlNames')
         .then(function (response) {
-    document.getElementById("boyNames").innerHTML = response.data[random];
+    document.getElementById("girlNames").innerHTML = response.data[random];
         })
 
     });
@@ -16,6 +16,3 @@ button1.addEventListener('click', function () {
     reset.addEventListener('click', function () {
         location.reload()
     });
-
-
-    
--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 var express = require('express');
 const bodyParser = require('body-parser')
-let counter = {name:"abdella",age:26};
+let name = [
+        'Aalam', 'Abdalah','Abdulkareem', 'Abednego', 'Adnan', 'Akeem'
+         ];
+
+var girlNames = [
+    'Aaliyah', 'Adila','Aizza', 'Alima', 'Aliyya', 'Altaf'
+];
+
+  
 
 var app = express();
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
 app.get('/names', function(req,res,) {
-    res.json(counter.name)
+        res.json(name)
+})  
+app.get('/girlNames', function(req,res,) {
+    res.json(girlNames)
 })  
 
 
